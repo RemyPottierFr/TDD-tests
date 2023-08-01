169b280760c243ab98a75628db6e68e2f806dce3
Lucas <ratouitlucaspro@gmail.com>
2023-08-01 17:20:22 +0200

    test/reduce

--- src/reduceByCategory.js
+++ src/reduceByCategory.js
@@ -4,7 +4,15 @@
  * @example reduceByCategory([{ category: "a", value: 1 }, { category: "b", value: 2 }, { category: "a", value: 3 }]) // [{ category: "a", value: 4 }, { category: "b", value: 2 }]
  */
 function reduceByCategory(arr) {
-  return arr;
+  return arr.reduce((acc, value) => {
+    let categoryExist = acc.find((item) => item.category === value.category);
+    if (categoryExist) {
+      categoryExist.value += value.value;
+    } else {
+      acc.push(value)
+    }
+    return acc
+  }, [])
 }
 
 module.exports = reduceByCategory;
